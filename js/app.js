c1a6d9b6c4e0cd876a5d86dbffe3c655045218e7
Anton Golubev <go_a_d_19@icloud.com>
2022-08-03 22:45:37 +0400

    drag-and-drop

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+placeholders.forEach((placeholder) => {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+})
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+
+
+
+/* Helpers */
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => {
+        event.target.classList.add('hide')
+    }, 0)
+} 
+    
+
+function dragend(event) {
+    event.target.classList.remove('hide', 'hold')
+}
+
+function dragover(event) {
+    console.log(event)
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
